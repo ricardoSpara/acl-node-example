948d8c1fafc821fccb8f9ef6a6af6e75e4c83808
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-03-21 17:29:04 -0300

    Adding middleware auth and jsonwebtoken

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const { Router } = require("express");
 
 const routes = new Router();
 
+const authMiddleware = require("./app/middlewares/auth");
+
+routes.use(authMiddleware);
+
 routes.get("/teste", (req, res) => {
   res.json({ ok: true });
 });
